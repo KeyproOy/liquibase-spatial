f2c8fcdf607652702496f33d18b4b519abb08a72
Alexei Pomelov <alexei.pomelov@keypro.fi>
2017-06-12 13:31:23 +0300

    Supported coordinate boundaries

--- src/main/java/liquibase/ext/spatial/change/CreateSpatialIndexChange.java
+++ src/main/java/liquibase/ext/spatial/change/CreateSpatialIndexChange.java
@@ -1,5 +1,7 @@
 package liquibase.ext.spatial.change;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,207 +23,286 @@ import liquibase.util.StringUtils;
  * The <code>CreateSpatialIndexChange</code> represents a database change to
  * create a spatial index.
  */
-@DatabaseChange(name = "createSpatialIndex",
-      description = "Creates a spatial index on an existing column or set of columns.",
-      priority = ChangeMetaData.PRIORITY_DEFAULT,
-      appliesTo = "index")
+@DatabaseChange(
+    name = "createSpatialIndex",
+    description = "Creates a spatial index on an existing column or set of columns.",
+    priority = ChangeMetaData.PRIORITY_DEFAULT,
+    appliesTo = "index"
+)
 public class CreateSpatialIndexChange extends AbstractChange
-      implements ChangeWithColumns<ColumnConfig> {
-   private String catalogName;
-   private String schemaName;
-   private String tableName;
-   private String indexName;
-   private String tablespace;
-   private List<ColumnConfig> columns = new ArrayList<ColumnConfig>();
-   private String geometryType;
-   private String srid;
+        implements ChangeWithColumns<ColumnConfig> {
+    private String catalogName;
+    private String schemaName;
+    private String tableName;
+    private String indexName;
+    private String tablespace;
+    private List<ColumnConfig> columns = new ArrayList<ColumnConfig>();
+    private String geometryType;
+    private String srid;
+    private String minX;
+    private String maxX;
+    private String minY;
+    private String maxY;
+    private String minZ;
+    private String maxZ;
+    private String tolerance;
 
-   /**
-    * Sets the database catalog name.
-    *
-    * @param catalogName
-    *           the catalog name.
-    */
-   public void setCatalogName(final String catalogName) {
-      this.catalogName = catalogName;
-   }
+    /**
+     * Sets the database catalog name.
+     *
+     * @param catalogName the catalog name.
+     */
+    public void setCatalogName(final String catalogName) {
+        this.catalogName = catalogName;
+    }
 
-   @DatabaseChangeProperty(description = "Name of the catalog")
-   public String getCatalogName() {
-      return this.catalogName;
-   }
+    @DatabaseChangeProperty(description = "Name of the catalog")
+    public String getCatalogName() {
+        return this.catalogName;
+    }
 
-   @DatabaseChangeProperty(mustEqualExisting = "index",
-         description = "Name of the index to create",
-         requiredForDatabase = "mysql, oracle, postgresql")
-   public String getIndexName() {
-      return this.indexName;
-   }
+    @DatabaseChangeProperty(mustEqualExisting = "index",
+            description = "Name of the index to create",
+            requiredForDatabase = "mysql, oracle, postgresql")
+    public String getIndexName() {
+        return this.indexName;
+    }
 
-   public void setIndexName(final String indexName) {
-      this.indexName = indexName;
-   }
+    public void setIndexName(final String indexName) {
+        this.indexName = indexName;
+    }
 
-   @DatabaseChangeProperty(mustEqualExisting = "index.schema")
-   public String getSchemaName() {
-      return this.schemaName;
-   }
+    @DatabaseChangeProperty(mustEqualExisting = "index.schema")
+    public String getSchemaName() {
+        return this.schemaName;
+    }
 
-   public void setSchemaName(final String schemaName) {
-      this.schemaName = schemaName;
-   }
+    public void setSchemaName(final String schemaName) {
+        this.schemaName = schemaName;
+    }
 
-   @DatabaseChangeProperty(mustEqualExisting = "index.table",
-         description = "Name of the table to add the index to",
-         exampleValue = "person",
-         requiredForDatabase = "all")
-   public String getTableName() {
-      return this.tableName;
-   }
+    @DatabaseChangeProperty(mustEqualExisting = "index.table",
+            description = "Name of the table to add the index to",
+            exampleValue = "person",
+            requiredForDatabase = "all")
+    public String getTableName() {
+        return this.tableName;
+    }
 
-   public void setTableName(final String tableName) {
-      this.tableName = tableName;
-   }
+    public void setTableName(final String tableName) {
+        this.tableName = tableName;
+    }
 
-   /**
-    * Returns the geometry type.
-    *
-    * @return the geometry type.
-    */
-   @DatabaseChangeProperty(description = "The Well-Known Text geometry type",
-         exampleValue = "POINT")
-   public String getGeometryType() {
-      return this.geometryType;
-   }
+    /**
+     * Returns the geometry type.
+     *
+     * @return the geometry type.
+     */
+    @DatabaseChangeProperty(description = "The Well-Known Text geometry type",
+            exampleValue = "POINT")
+    public String getGeometryType() {
+        return this.geometryType;
+    }
 
-   /**
-    * Sets the geometry type.
-    *
-    * @param geometryType
-    *           the geometry type.
-    */
-   public void setGeometryType(final String geometryType) {
-      this.geometryType = geometryType;
-   }
+    /**
+     * Sets the geometry type.
+     *
+     * @param geometryType the geometry type.
+     */
+    public void setGeometryType(final String geometryType) {
+        this.geometryType = geometryType;
+    }
 
-   /**
-    * Returns the srid.
-    *
-    * @return the srid.
-    */
-   @DatabaseChangeProperty(
-         description = "The Spatial Reference ID of the indexed data.  An EPSG SRID is assumed.",
-         exampleValue = "4326",
-         requiredForDatabase = "derby, h2")
-   public String getSrid() {
-      return this.srid;
-   }
+    /**
+     * Returns the srid.
+     *
+     * @return the srid.
+     */
+    @DatabaseChangeProperty(
+            description = "The Spatial Reference ID of the indexed data.  An EPSG SRID is assumed.",
+            exampleValue = "4326",
+            requiredForDatabase = "derby, h2")
+    public String getSrid() {
+        return this.srid;
+    }
 
-   /**
-    * Sets the srid.
-    *
-    * @param srid
-    *           the srid.
-    */
-   public void setSrid(final String srid) {
-      this.srid = srid;
-   }
+    /**
+     * Sets the srid.
+     *
+     * @param srid the srid.
+     */
+    public void setSrid(final String srid) {
+        this.srid = srid;
+    }
 
-   @Override
-   @DatabaseChangeProperty(mustEqualExisting = "index.column",
-         description = "Column(s) to add to the index",
-         requiredForDatabase = "all")
-   public List<ColumnConfig> getColumns() {
-      if (this.columns == null) {
-         return new ArrayList<ColumnConfig>();
-      }
-      return this.columns;
-   }
+    @DatabaseChangeProperty
+    public String getMinX() {
+        return minX;
+    }
 
-   @Override
-   public void setColumns(final List<ColumnConfig> columns) {
-      this.columns = columns;
-   }
+    public void setMinX(String minX) {
+        this.minX = minX;
+    }
 
-   @Override
-   public void addColumn(final ColumnConfig column) {
-      this.columns.add(column);
-   }
+    @DatabaseChangeProperty
+    public String getMaxX() {
+        return maxX;
+    }
 
-   @DatabaseChangeProperty(description = "Tablepace to create the index in.")
-   public String getTablespace() {
-      return this.tablespace;
-   }
+    public void setMaxX(String maxX) {
+        this.maxX = maxX;
+    }
 
-   public void setTablespace(final String tablespace) {
-      this.tablespace = tablespace;
-   }
+    @DatabaseChangeProperty
+    public String getMinY() {
+        return minY;
+    }
 
-   /**
-    * @see liquibase.change.AbstractChange#validate(liquibase.database.Database)
-    */
-   @Override
-   public ValidationErrors validate(final Database database) {
-      final ValidationErrors validationErrors = new ValidationErrors();
-      if (this.srid != null) {
-         if (!this.srid.matches("[0-9]+")) {
-            validationErrors.addError("The SRID must be numeric");
-         }
-      }
+    public void setMinY(String minY) {
+        this.minY = minY;
+    }
 
-      if (!validationErrors.hasErrors()) {
-         validationErrors.addAll(super.validate(database));
-      }
+    @DatabaseChangeProperty
+    public String getMaxY() {
+        return maxY;
+    }
 
-      return validationErrors;
-   }
+    public void setMaxY(String maxY) {
+        this.maxY = maxY;
+    }
 
-   @Override
-   public String getConfirmationMessage() {
-      final StringBuilder message = new StringBuilder("Spatial index");
-      if (StringUtils.trimToNull(getIndexName()) != null) {
-         message.append(' ').append(getIndexName().trim());
-      }
-      message.append(" created");
-      if (StringUtils.trimToNull(getTableName()) != null) {
-         message.append(" on ").append(getTableName().trim());
-      }
-      return message.toString();
-   }
+    @DatabaseChangeProperty
+    public String getMinZ() {
+        return minZ;
+    }
 
-   @Override
-   public SqlStatement[] generateStatements(final Database database) {
-      final String[] columns = new String[this.columns.size()];
-      int ii = 0;
-      for (final ColumnConfig columnConfig : this.columns) {
-         columns[ii++] = columnConfig.getName();
-      }
+    public void setMinZ(String minZ) {
+        this.minZ = minZ;
+    }
 
-      // Parse the string SRID into an integer.
-      Integer srid = null;
-      if (getSrid() != null) {
-         srid = Integer.valueOf(getSrid());
-      }
+    @DatabaseChangeProperty
+    public String getMaxZ() {
+        return maxZ;
+    }
 
-      final CreateSpatialIndexStatement statement = new CreateSpatialIndexStatement(
-            getIndexName(), getCatalogName(), getSchemaName(), getTableName(), columns,
-            getTablespace(), getGeometryType(), srid);
-      return new SqlStatement[] { statement };
-   }
+    public void setMaxZ(String maxZ) {
+        this.maxZ = maxZ;
+    }
 
-   @Override
-   protected Change[] createInverses() {
-      final DropSpatialIndexChange inverse = new DropSpatialIndexChange();
-      inverse.setCatalogName(getCatalogName());
-      inverse.setSchemaName(getSchemaName());
-      inverse.setTableName(getTableName());
-      inverse.setIndexName(getIndexName());
+    @DatabaseChangeProperty
+    public String getTolerance() {
+        return tolerance;
+    }
 
-      return new Change[] { inverse };
-   }
+    public void setTolerance(String tolerance) {
+        this.tolerance = tolerance;
+    }
 
-   @Override
-   public String getSerializedObjectNamespace() {
-      return XmlConstants.SPATIAL_CHANGELOG_NAMESPACE;
-   }
+    @Override
+    @DatabaseChangeProperty(mustEqualExisting = "index.column",
+            description = "Column(s) to add to the index",
+            requiredForDatabase = "all")
+    public List<ColumnConfig> getColumns() {
+        if (this.columns == null) {
+            return new ArrayList<ColumnConfig>();
+        }
+        return this.columns;
+    }
+
+    @Override
+    public void setColumns(final List<ColumnConfig> columns) {
+        this.columns = columns;
+    }
+
+    @Override
+    public void addColumn(final ColumnConfig column) {
+        this.columns.add(column);
+    }
+
+    @DatabaseChangeProperty(description = "Tablepace to create the index in.")
+    public String getTablespace() {
+        return this.tablespace;
+    }
+
+    public void setTablespace(final String tablespace) {
+        this.tablespace = tablespace;
+    }
+
+    /**
+     * @see liquibase.change.AbstractChange#validate(liquibase.database.Database)
+     */
+    @Override
+    public ValidationErrors validate(final Database database) {
+        final ValidationErrors validationErrors = new ValidationErrors();
+        if (this.srid != null) {
+            if (!this.srid.matches("[0-9]+")) {
+                validationErrors.addError("The SRID must be numeric");
+            }
+        }
+
+        if (!validationErrors.hasErrors()) {
+            validationErrors.addAll(super.validate(database));
+        }
+
+        return validationErrors;
+    }
+
+    @Override
+    public String getConfirmationMessage() {
+        final StringBuilder message = new StringBuilder("Spatial index");
+        if (StringUtils.trimToNull(getIndexName()) != null) {
+            message.append(' ').append(getIndexName().trim());
+        }
+        message.append(" created");
+        if (StringUtils.trimToNull(getTableName()) != null) {
+            message.append(" on ").append(getTableName().trim());
+        }
+        return message.toString();
+    }
+
+    @Override
+    public SqlStatement[] generateStatements(final Database database) {
+        final String[] columns = new String[this.columns.size()];
+        int ii = 0;
+        for (final ColumnConfig columnConfig : this.columns) {
+            columns[ii++] = columnConfig.getName();
+        }
+
+        // Parse the string SRID into an integer.
+        Integer srid = null;
+        if (getSrid() != null) {
+            srid = Integer.valueOf(getSrid());
+        }
+
+        BigDecimal minx = new BigDecimal(minX != null ? minX : "30000");
+        BigDecimal maxx = new BigDecimal(maxX != null ? maxX : "80000");
+        BigDecimal miny = new BigDecimal(minY != null ? minY : "10000");
+        BigDecimal maxy = new BigDecimal(maxY != null ? maxY : "30000");
+
+        BigDecimal minz = (minZ != null) ? new BigDecimal(minZ) : null;
+        BigDecimal maxz = (maxZ != null) ? new BigDecimal(maxZ) : null;
+
+        BigDecimal tole = new BigDecimal(tolerance != null ? tolerance : "0.01");
+
+        final CreateSpatialIndexStatement statement = new CreateSpatialIndexStatement(
+                getIndexName(), getCatalogName(), getSchemaName(), getTableName(), columns,
+                getTablespace(), getGeometryType(), srid, minx, maxx, miny, maxy, minz, maxz, tole);
+        return new SqlStatement[]{statement};
+    }
+
+    @Override
+    protected Change[] createInverses() {
+        final DropSpatialIndexChange inverse = new DropSpatialIndexChange();
+        inverse.setCatalogName(getCatalogName());
+        inverse.setSchemaName(getSchemaName());
+        inverse.setTableName(getTableName());
+        inverse.setIndexName(getIndexName());
+
+        return new Change[]{inverse};
+    }
+
+    @Override
+    public String getSerializedObjectNamespace() {
+        return XmlConstants.SPATIAL_CHANGELOG_NAMESPACE;
+    }
 }
--- src/main/java/liquibase/ext/spatial/statement/CreateSpatialIndexStatement.java
+++ src/main/java/liquibase/ext/spatial/statement/CreateSpatialIndexStatement.java
@@ -2,123 +2,206 @@ package liquibase.ext.spatial.statement;
 
 import liquibase.statement.AbstractSqlStatement;
 
+import java.math.BigDecimal;
+
 /**
  * <code>CreateSpatialIndexStatement</code> represents a
  * <code>CREATE SPATIAL INDEX</code> statement.
  */
 public class CreateSpatialIndexStatement extends AbstractSqlStatement {
 
-   private final String tableCatalogName;
-   private final String tableSchemaName;
-   private final String indexName;
-   private final String tableName;
-   private final String[] columns;
-   private String tablespace;
+    private final String indexName;
+    private final String tableCatalogName;
+    private final String tableSchemaName;
+    private final String tableName;
+    private final String[] columns;
+    private String tablespace;
 
-   /** The WKT geometry type (e.g. Geometry, Point, etc). */
-   private String geometryType;
+    /**
+     * The WKT geometry type (e.g. Geometry, Point, etc).
+     */
+    private String geometryType;
 
-   /** The Spatial Reference ID (e.g. 4326). */
-   private Integer srid;
+    /**
+     * The Spatial Reference ID (e.g. 4326).
+     */
+    private Integer srid;
 
-   /**
-    * Constructs a new instance with the given parameters.
-    * 
-    * @param indexName
-    *           the name of the index to create.
-    * @param tableCatalogName
-    *           the optional table's catalog name.
-    * @param tableSchemaName
-    *           the optional table's schema name.
-    * @param tableName
-    *           the table name.
-    * @param columns
-    *           the array of column names.
-    * @param tablespace
-    *           the optional table space name.
-    * @param geometryType
-    *           the optional geometry type.
-    * @param srid
-    *           the optional Spatial Reference ID.
-    */
-   public CreateSpatialIndexStatement(final String indexName,
-         final String tableCatalogName, final String tableSchemaName,
-         final String tableName, final String[] columns, final String tablespace,
-         final String geometryType, final Integer srid) {
-      this.indexName = indexName;
-      this.tableCatalogName = tableCatalogName;
-      this.tableSchemaName = tableSchemaName;
-      this.tableName = tableName;
-      this.columns = columns.clone();
-      this.tablespace = tablespace;
-      this.geometryType = geometryType;
-      this.srid = srid;
-   }
+    private BigDecimal minX;
+    private BigDecimal maxX;
+    private BigDecimal minY;
+    private BigDecimal maxY;
+    private BigDecimal minZ;
+    private BigDecimal maxZ;
+    private BigDecimal tolerance;
 
-   public String getTableCatalogName() {
-      return this.tableCatalogName;
-   }
+    /**
+     * Constructs a new instance with the given parameters.
+     *
+     * @param indexName        the name of the index to create.
+     * @param tableCatalogName the optional table's catalog name.
+     * @param tableSchemaName  the optional table's schema name.
+     * @param tableName        the table name.
+     * @param columns          the array of column names.
+     * @param tablespace       the optional table space name.
+     * @param geometryType     the optional geometry type.
+     * @param srid             the optional Spatial Reference ID.
+     */
+    public CreateSpatialIndexStatement(final String indexName, final String tableCatalogName, final String tableSchemaName,
+                                       final String tableName, final String[] columns, final String tablespace,
+                                       final String geometryType, final Integer srid) {
+        this.indexName = indexName;
+        this.tableCatalogName = tableCatalogName;
+        this.tableSchemaName = tableSchemaName;
+        this.tableName = tableName;
+        this.columns = columns.clone();
+        this.tablespace = tablespace;
+        this.geometryType = geometryType;
+        this.srid = srid;
+    }
 
-   public String getTableSchemaName() {
-      return this.tableSchemaName;
-   }
+    public CreateSpatialIndexStatement(String indexName, String tableCatalogName, String tableSchemaName,
+                                       String tableName, String[] columns, String tablespace,
+                                       String geometryType, Integer srid,
+                                       BigDecimal minX, BigDecimal maxX,
+                                       BigDecimal minY, BigDecimal maxY,
+                                       BigDecimal minZ, BigDecimal maxZ,
+                                       BigDecimal tolerance) {
+        this.indexName = indexName;
+        this.tableCatalogName = tableCatalogName;
+        this.tableSchemaName = tableSchemaName;
+        this.tableName = tableName;
+        this.columns = columns.clone();
+        this.tablespace = tablespace;
+        this.geometryType = geometryType;
+        this.srid = srid;
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minY = minY;
+        this.maxY = maxY;
+        this.minZ = minZ;
+        this.maxZ = maxZ;
+        this.tolerance = tolerance;
+    }
 
-   public String getIndexName() {
-      return this.indexName;
-   }
+    public String getTableCatalogName() {
+        return this.tableCatalogName;
+    }
 
-   public String getTableName() {
-      return this.tableName;
-   }
+    public String getTableSchemaName() {
+        return this.tableSchemaName;
+    }
 
-   public String[] getColumns() {
-      return this.columns;
-   }
+    public String getIndexName() {
+        return this.indexName;
+    }
 
-   public String getTablespace() {
-      return this.tablespace;
-   }
+    public String getTableName() {
+        return this.tableName;
+    }
 
-   public CreateSpatialIndexStatement setTablespace(final String tablespace) {
-      this.tablespace = tablespace;
-      return this;
-   }
+    public String[] getColumns() {
+        return this.columns;
+    }
 
-   /**
-    * Sets the WKT geometry type (e.g. Geometry, Point, etc).
-    * 
-    * @param geometryType
-    *           the geometry type.
-    */
-   public void setGeometryType(final String geometryType) {
-      this.geometryType = geometryType;
-   }
+    public String getTablespace() {
+        return this.tablespace;
+    }
 
-   /**
-    * Returns the WKT geometry type (e.g. Geometry, Point, etc).
-    * 
-    * @return the geometry type.
-    */
-   public String getGeometryType() {
-      return this.geometryType;
-   }
+    public CreateSpatialIndexStatement setTablespace(final String tablespace) {
+        this.tablespace = tablespace;
+        return this;
+    }
 
-   /**
-    * Sets the Spatial Reference ID (e.g. 4326).
-    * 
-    * @param srid
-    *           the SRID.
-    */
-   public void setSrid(final Integer srid) {
-      this.srid = srid;
-   }
+    /**
+     * Sets the WKT geometry type (e.g. Geometry, Point, etc).
+     *
+     * @param geometryType the geometry type.
+     */
+    public void setGeometryType(final String geometryType) {
+        this.geometryType = geometryType;
+    }
 
-   /**
-    * Returns the Spatial Reference ID (e.g. 4326).
-    * 
-    * @return the SRID.
-    */
-   public Integer getSrid() {
-      return this.srid;
-   }
+    /**
+     * Returns the WKT geometry type (e.g. Geometry, Point, etc).
+     *
+     * @return the geometry type.
+     */
+    public String getGeometryType() {
+        return this.geometryType;
+    }
+
+    /**
+     * Sets the Spatial Reference ID (e.g. 4326).
+     *
+     * @param srid the SRID.
+     */
+    public void setSrid(final Integer srid) {
+        this.srid = srid;
+    }
+
+    /**
+     * Returns the Spatial Reference ID (e.g. 4326).
+     *
+     * @return the SRID.
+     */
+    public Integer getSrid() {
+        return this.srid;
+    }
+
+    public BigDecimal getMinX() {
+        return minX;
+    }
+
+    public void setMinX(BigDecimal minX) {
+        this.minX = minX;
+    }
+
+    public BigDecimal getMaxX() {
+        return maxX;
+    }
+
+    public void setMaxX(BigDecimal maxX) {
+        this.maxX = maxX;
+    }
+
+    public BigDecimal getMinY() {
+        return minY;
+    }
+
+    public void setMinY(BigDecimal minY) {
+        this.minY = minY;
+    }
+
+    public BigDecimal getMaxY() {
+        return maxY;
+    }
+
+    public void setMaxY(BigDecimal maxY) {
+        this.maxY = maxY;
+    }
+
+    public BigDecimal getMinZ() {
+        return minZ;
+    }
+
+    public void setMinZ(BigDecimal minZ) {
+        this.minZ = minZ;
+    }
+
+    public BigDecimal getMaxZ() {
+        return maxZ;
+    }
+
+    public void setMaxZ(BigDecimal maxZ) {
+        this.maxZ = maxZ;
+    }
+
+    public BigDecimal getTolerance() {
+        return tolerance;
+    }
+
+    public void setTolerance(BigDecimal tolerance) {
+        this.tolerance = tolerance;
+    }
 }
